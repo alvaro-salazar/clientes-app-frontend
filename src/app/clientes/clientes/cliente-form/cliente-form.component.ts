6644bea0aa8745920c532abdbf21d8c60254ae67
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2024-11-13 00:29:23 -0500

    fix: adding cliente form added

---
+++ src/app/clientes/clientes/cliente-form/cliente-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Cliente} from '../model/cliente';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-cliente-form',
+  templateUrl: './cliente-form.component.html'
+})
+export class ClienteFormComponent {
+  @Input() cliente: Cliente = { id: 0, nombre: '', apellido: '', email: '', createAt: '', foto: '', region: { id: 0, nombre: '' } };
+  @Output() onSave = new EventEmitter<Cliente>();
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  cancel(): void {
+    this.activeModal.dismiss();
+  }
+
+
+  saveCliente(): void {
+    this.onSave.emit(this.cliente);
+  }
+}
